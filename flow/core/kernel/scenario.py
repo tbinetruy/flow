388ce81c1249b4ef41eb61a6b02f7c84da6ee1eb
Kaila <kaila_cappello@berkeley.edu>
2018-09-28 18:01:13 -0700

    Add scenario kernel draft script.

--- flow/core/kernel/scenario.py
+++ flow/core/kernel/scenario.py
@@ -2,6 +2,35 @@ class Scenario:
     def __init__(self, kernel):
         self.K = kernel
 
-    def get_speed(self, arg1, arg2):
-        return self.K.vehicle.getSpeed(arg1, arg2)
+        
+   def get_edge_list(self):
+        return self.K.edge.getIDList()
+
+    def edge_length(self):
+        return 
+
+    def speed_limit(self):
+        return
+
+    def num_lanes(self):
+        return
+
+    def max_speed(self):
+        return
+
+    def get_junction_list(self):
+        return 
+
+    def get_edge(self):
+        return 
+
+    def get_x(self):
+        return
+
+    def next_edge(self):
+        return
+
+    def prev_edge(self):
+        return
+
 
