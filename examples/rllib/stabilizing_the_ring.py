0fb60807ed658557467b056bf6d9e1440235502c
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-07 15:28:52 -0800

    removed custom start pos call

--- examples/rllib/stabilizing_the_ring.py
+++ examples/rllib/stabilizing_the_ring.py
@@ -84,7 +84,7 @@ flow_params = dict(
 
     # parameters specifying the positioning of vehicles upon initialization/
     # reset (see flow.core.params.InitialConfig)
-    initial=InitialConfig(bunching=20.0, spacing='custom'),
+    initial=InitialConfig(bunching=20.0),
 )
 
 
