1e792cf0f8022308825768663f8494840ff01dd8
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-06 23:01:31 -0800

    revert no collide

--- examples/rllib/multiagent_exps/multiagent_merge.py
+++ examples/rllib/multiagent_exps/multiagent_merge.py
@@ -52,12 +52,12 @@ vehicles.add(
     acceleration_controller=(IDMController, {
         "noise": 0.2
     }),
-    speed_mode=9,
+    speed_mode="no_collide",
     num_vehicles=5)
 vehicles.add(
     veh_id="rl",
     acceleration_controller=(RLController, {}),
-    speed_mode=9,
+    speed_mode="no_collide",
     num_vehicles=0)
 
 # Vehicles are introduced from both sides of merge, with RL vehicles entering
